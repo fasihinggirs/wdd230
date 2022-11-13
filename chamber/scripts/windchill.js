15c162b87d5091f39a56fe4e15b2e7b7b088732e
nuelnggiku <ngg22001@byui.edu>
2022-11-14 06:11:15 +0700

    FINISHED

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -10,6 +10,10 @@ const speedmH = speedkh / 1.609;
 console.log(tempF);
 console.log(speedmH);
 
+//toLacalDateString
+const options = {weekday: 'long', day: 'numeric', month:'long', year:'numeric'};
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString ('en-US', options);
+
 
 if (tempF <= 50 && speedmH > 3) {
     const f = 35.74 + 0.6215 * tempF - 35.75 * (speedmH ** 0.16) + 0.4275 * tempF * (speedmH ** 0.16)
