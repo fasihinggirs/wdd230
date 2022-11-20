d86f2295842cdb67919ade5a3b24c41abff3cb52
nuelnggiku <ngg22001@byui.edu>
2022-11-20 20:47:20 +0700

    Saya pusing

---
+++ lesson10/scripts/weatherapi.js
@@ -0,0 +1,13 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=3523349&units=imperial&appid=503438ab4835fffec0356666d1a3c88f';
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
+    const desc = jsObject.weather[0].description;
+    document.getElementById('imagesrc').textContent = imagesrc;
+    document.getElementById('icon').setAttribute('src', imagesrc);
+    document.getElementById('icon').setAttribute('alt', desc);
+  });
