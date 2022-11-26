00c0565e1fb199991cb1d7eb8b464910b16d8e3e
nuelnggiku <ngg22001@byui.edu>
2022-11-26 23:38:32 +0700

    YEAH ILANG

--- chamber/scripts/currentdate.js
+++
@@ -1,4 +0,0 @@
-//toLacalDateString
-const options = {weekday: 'long', day: 'numeric', month:'long', year:'numeric'};
-document.getElementById('currentdate').textContent = new Date().toLocaleDateString ('en-US', options);
-
